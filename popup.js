af03f7b115cefff17722a05bbbe11da49055e5ce
cocobball <corymasri@gmail.com>
2023-08-10 12:58:19 -0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,21 +1,27 @@
 document.addEventListener('DOMContentLoaded', function() {
+  // Add event listener to checkHeaders button
   document.getElementById('checkHeaders').addEventListener('click', function() {
     chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
       chrome.tabs.sendMessage(tabs[0].id, { action: "checkHeaders" }, function(response) {
         var resultDiv = document.getElementById('result');
+        // Meta Title
         var metaTitleLength = response.metaTitle ? response.metaTitle.length : 0;
         resultDiv.innerHTML = '<div class="section"><span class="title">Meta Title:</span> ' + (response.metaTitle || 'Not found') + ' <span style="color:' + (metaTitleLength <= 60 ? 'green' : 'red') + '">' + metaTitleLength + '/60</span></div>';
+        // Meta Description
         var metaDescriptionLength = response.metaDescription ? response.metaDescription.length : 0;
         resultDiv.innerHTML += '<div class="section"><span class="title">Meta Description:</span> ' + (response.metaDescription || 'Not found') + ' <span style="color:' + (metaDescriptionLength <= 160 ? 'green' : 'red') + '">' + metaDescriptionLength + '/160</span></div>';
+        // H1 tag
         var h1Length = response.h1 ? response.h1.length : 0;
         resultDiv.innerHTML += '<div class="section"><span class="title">H1 tag:</span> ' + (response.h1 || 'Not found') + ' <span style="color:' + (h1Length <= 70 ? 'green' : 'red') + '">' + h1Length + '/70</span></div>';
+        // H2 tags
         var h2TagsContent = response.h2s.map((h2, index) => {
           var h2Length = h2.length;
           return h2 + ' <span style="color:' + (h2Length <= 70 ? 'green' : 'red') + '">' + h2Length + '/70</span>';
         }).join('<br>');
         resultDiv.innerHTML += '<div class="section"><span class="title">H2 tags:</span><br>' + (h2TagsContent || 'Not found') + '</div>';
+        // Canonical Tag
         resultDiv.innerHTML += '<div class="section"><span class="title">Canonical Tag:</span> <span style="color:' + (response.canonicalStatus ? 'green' : 'red') + '">' + (response.canonicalStatus ? 'Self-referencing (Good)' : 'Not self-referencing (Bad)') + '</span></div>';
-        // New code for additional functionalities
+        // Additional functionalities
         resultDiv.innerHTML += '<div class="section"><span class="title">Meta Robots:</span> ' + (response.robotsMeta || 'Not found') + '</div>';
         resultDiv.innerHTML += '<div class="section"><span class="title">Images with ALT:</span> ' + response.imagesWithAlt + '</div>';
         resultDiv.innerHTML += '<div class="section"><span class="title">Images without ALT:</span> ' + response.imagesWithoutAlt + '</div>';
@@ -27,6 +33,7 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   });
 
+  // Add event listener to toggleImageHover checkbox
   var toggleImageHover = document.getElementById('imageHoverToggle');
   toggleImageHover.addEventListener('change', function() {
     var isEnabled = this.checked;
@@ -41,11 +48,29 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   });
 
+  // Add event listener to toggleHeadingPopup checkbox
+  var toggleHeadingPopup = document.getElementById('headingPopupToggle');
+  toggleHeadingPopup.addEventListener('change', function() {
+    var isEnabled = this.checked;
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, { action: 'toggleHeadingPopup', enabled: isEnabled }, function(response) {
+        if (response && response.success) {
+          console.log('Heading popup toggle updated successfully!');
+        } else {
+          console.error('Failed to update heading popup toggle:', response ? response.message : 'No response');
+        }
+      });
+    });
+  });
+
+  // Initialize toggle states
   var isImageHoverEnabled = true;
   var isHeadingPopupEnabled = true;
 
+  // Add message listener for checkHeaders action
   chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if (request.action === "checkHeaders") {
+      // Get page elements
       var h1Tag = document.querySelector('h1');
       var allH2Tags = document.querySelectorAll('h2');
       var h2Texts = Array.from(allH2Tags).map(tag => tag.textContent);
@@ -55,7 +80,7 @@ document.addEventListener('DOMContentLoaded', function() {
       var canonicalURL = canonicalTag ? canonicalTag.href : null;
       var isSelfReferencing = canonicalURL === window.location.href;
 
-      // New code for additional functionalities
+      // Additional functionalities
       var allImages = document.querySelectorAll('img');
       var imagesWithAlt = Array.from(allImages).filter(img => img.alt && img.alt.trim() !== '').length;
       var imagesWithoutAlt = allImages.length - imagesWithAlt;
@@ -66,6 +91,7 @@ document.addEventListener('DOMContentLoaded', function() {
       var sitemapTag = document.querySelector('link[rel="sitemap"]');
       var robotsTxtLink = window.location.origin + "/robots.txt";
 
+      // Send response
       sendResponse({
         h1: h1Tag ? h1Tag.textContent : null,
         h2s: h2Texts,
@@ -83,17 +109,20 @@ document.addEventListener('DOMContentLoaded', function() {
     }
   });
 
+  // Add event listener for mouseover event
   document.addEventListener('mouseover', function(e) {
     if (!isImageHoverEnabled) return;
 
     var target = e.target;
     if (target.tagName.toLowerCase() === 'img') {
+      // Create tooltip content
       var info = "Alt Text: " + target.alt + "\n";
       info += "Width: " + target.width + "px\n";
       info += "Height: " + target.height + "px\n";
       var isResized = target.naturalWidth !== target.width || target.naturalHeight !== target.height;
       var resizedInfo = "Resized: " + (isResized ? "Yes" : "No");
 
+      // Create tooltip element
       var tooltip = document.createElement('div');
       tooltip.style.position = 'fixed';
       tooltip.style.left = e.pageX + 'px';
@@ -109,8 +138,8 @@ document.addEventListener('DOMContentLoaded', function() {
 
       tooltip.innerHTML = info + resizedInfo;
 
+      // Add tooltip to the body and remove it on mouseout
       document.body.appendChild(tooltip);
-
       target.addEventListener('mouseout', function() {
         document.body.removeChild(tooltip);
       });
@@ -134,66 +163,81 @@ document.addEventListener('DOMContentLoaded', function() {
 
       headingPopup.innerHTML = headingInfo;
 
+      // Add heading popup to the body and remove it on mouseout
       document.body.appendChild(headingPopup);
-
       target.addEventListener('mouseout', function() {
         document.body.removeChild(headingPopup);
       });
     }
   });
 
+  // Add event listener for checkGoogleIndex button
   document.getElementById('checkGoogleIndex').addEventListener('click', function() {
     chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
       chrome.tabs.create({ url: 'https://www.google.com/search?q=site:' + tabs[0].url });
     });
   });
 
+  // Add event listener for checkPageSpeed button
   document.getElementById('checkPageSpeed').addEventListener('click', function() {
     chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
       chrome.tabs.create({ url: 'https://developers.google.com/speed/pagespeed/insights/?url=' + tabs[0].url });
     });
   });
 
+  // Add event listener for checkGoogleCache button
   document.getElementById('checkGoogleCache').addEventListener('click', function() {
     chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
       chrome.tabs.create({ url: 'https://webcache.googleusercontent.com/search?q=cache:' + tabs[0].url });
     });
   });
 
-// Define the custom prompts
-var customPrompts = {
-  'customPrompt1': `I want you to become my Prompt Creator. Your goal is to help me craft the best possible prompt for my needs. The prompt will be used by you, ChatGPT. You will follow the following process:
-1. Your first response will be to ask me what the prompt should be about. I will provide my answer, but we will need to improve it through continual iterations by going through the next steps.
-2. Based on my input, you will generate 3 sections. a) Revised prompt (provide your rewritten prompt. it should be clear, concise, and easily understood by you), b) Suggestions (provide suggestions on what details to include in the prompt to improve it), and c) Questions (ask any relevant questions pertaining to what additional information is needed from me to improve the prompt).
-3. We will continue this iterative process with me providing additional information to you and you updating the prompt in the Revised prompt section until it's complete.`,
-  'customPrompt2': 'This is your custom text for Prompt 2',
-};
+  // Define custom prompts
+  var customPrompts = {
+    'customPrompt1': `I want you to become my Prompt Creator. Your goal is to help me craft the best possible prompt for my needs... (continues)`
+    // Add more custom prompts here if needed
+  };
 
-document.getElementById('insertPrompt').addEventListener('click', function() {
-  // Get the selected value from the dropdown
-  var selectedValue = document.getElementById('prompts').value;
+  // Add event listener for insertPrompt button
+  document.getElementById('insertPrompt').addEventListener('click', function() {
+    // Get the selected value from the dropdown
+    var selectedValue = document.getElementById('prompts').value;
 
-  // Get the corresponding custom text
-  var selectedPromptText = customPrompts[selectedValue];
+    // Get the corresponding custom text
+    var selectedPromptText = customPrompts[selectedValue];
 
-  // Get the textarea element by its ID
-  var textarea = document.getElementById('prompt-textarea');
+    // Get the textarea element by its ID
+    var textarea = document.getElementById('prompt-textarea');
 
-  // Insert the selected custom prompt text into the textarea
-  textarea.value = selectedPromptText;
-});
-  // code to perform the schema check
-document.getElementById('schemaCheck').addEventListener('click', function() {
-  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, { action: "performSchemaCheck" }, function(response) {
-      var resultDiv = document.getElementById('result');
-      if (response) {
-        var formattedSchemaData = JSON.stringify(response, null, 2);
-        resultDiv.innerHTML += '<div class="section"><span class="title">Schema Data:</span><pre>' + formattedSchemaData + '</pre></div>';
-      } else {
-        resultDiv.innerHTML += '<div class="section"><span class="title">Schema Data:</span> Not found</div>';
-      }
+    // Insert the selected custom prompt text into the textarea
+    textarea.value = selectedPromptText;
+  });
+
+  // Add event listener for schemaCheck button
+  document.getElementById('schemaCheck').addEventListener('click', function() {
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, { action: "performSchemaCheck" }, function(response) {
+        var resultDiv = document.getElementById('result');
+        if (response) {
+          var schemaJSON = JSON.stringify(response, null, 2);
+          resultDiv.innerHTML = '<pre>' + schemaJSON + '</pre>';
+        } else {
+          resultDiv.innerHTML = 'No schema data found on this page.';
+        }
+      });
     });
   });
-});
+
+  // Load initial toggle states
+  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, { action: 'toggleImageHover', enabled: null }, function(response) {
+      isImageHoverEnabled = response.enabled;
+      toggleImageHover.checked = isImageHoverEnabled;
+    });
+
+    chrome.tabs.sendMessage(tabs[0].id, { action: 'toggleHeadingPopup', enabled: null }, function(response) {
+      isHeadingPopupEnabled = response.enabled;
+      toggleHeadingPopup.checked = isHeadingPopupEnabled;
+    });
+  });
 });
