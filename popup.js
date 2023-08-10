310af9b72b4f16ddebe9646fef160126d00a9ed4
cocobball <corymasri@gmail.com>
2023-08-10 11:27:36 -0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -160,23 +160,26 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   });
 
-// Define the predefined prompts
-var prompts = {
-  'Prompt 1': 'This is the text for Prompt 1',
-  'Prompt 2': 'This is the text for Prompt 2',
+// Define the custom prompts
+var customPrompts = {
+  'customPrompt1': `I want you to become my Prompt Creator. Your goal is to help me craft the best possible prompt for my needs. The prompt will be used by you, ChatGPT. You will follow the following process:
+1. Your first response will be to ask me what the prompt should be about. I will provide my answer, but we will need to improve it through continual iterations by going through the next steps.
+2. Based on my input, you will generate 3 sections. a) Revised prompt (provide your rewritten prompt. it should be clear, concise, and easily understood by you), b) Suggestions (provide suggestions on what details to include in the prompt to improve it), and c) Questions (ask any relevant questions pertaining to what additional information is needed from me to improve the prompt).
+3. We will continue this iterative process with me providing additional information to you and you updating the prompt in the Revised prompt section until it's complete.`,
+  'customPrompt2': 'This is your custom text for Prompt 2',
 };
 
 document.getElementById('insertPrompt').addEventListener('click', function() {
   // Get the selected value from the dropdown
   var selectedValue = document.getElementById('prompts').value;
 
-  // Get the corresponding predefined text
-  var selectedPromptText = prompts[selectedValue];
+  // Get the corresponding custom text
+  var selectedPromptText = customPrompts[selectedValue];
 
   // Get the textarea element by its ID
   var textarea = document.getElementById('prompt-textarea');
 
-  // Insert the selected prompt text into the textarea
+  // Insert the selected custom prompt text into the textarea
   textarea.value = selectedPromptText;
 });
   // code to perform the schema check
