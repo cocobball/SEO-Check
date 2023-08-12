0807c4e54356f0db3574e63003f2b65c2a25518d
cocobball <corymasri@gmail.com>
2023-08-12 13:35:58 -0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -243,3 +243,18 @@ var customPrompts = {
     });
   });
 });
+// Added new Check Frameworks section 
+document.getElementById('checkFrameworks').addEventListener('click', function() {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    chrome.tabs.sendMessage(tabs[0].id, { action: 'checkFrameworks' }, (response) => {
+      // Display the result
+      const resultDiv = document.getElementById('frameworksResult');
+      resultDiv.style.display = 'block';
+      if (response && response.gtmId) {
+        resultDiv.innerHTML = `Google Tag Manager: <strong>${response.gtmId}</strong>`;
+      } else {
+        resultDiv.innerHTML = 'Google Tag Manager: <strong style="color:red;">No</strong>';
+      }
+    });
+  });
+});
